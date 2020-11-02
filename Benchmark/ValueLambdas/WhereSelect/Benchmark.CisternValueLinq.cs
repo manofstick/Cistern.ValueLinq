2d696201deceebd78ce3d56ae6d592ff71e0231a
Reegeek <10356780+reegeek@users.noreply.github.com>
2020-11-02 17:25:04 +0100

    Add bench on foreach on WhereSelect

---
+++ Benchmark/ValueLambdas/WhereSelect/Benchmark.CisternValueLinq.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Attributes;
+using Cistern.ValueLinq;
+
+namespace Cistern.Benchmarks.ValueLambdas.WhereSelect
+{
+    partial class Benchmark
+    {
+        struct HalveAnInt : IFunc<int, int> { public int Invoke(int t) => t / 2; } 
+        struct FilterEvenInts : IFunc<int, bool> { public bool Invoke(int t) => (t & 1) == 0; }
+        
+        [Benchmark]
+        public int CisternValueLinq()
+        {
+            var enumerable = _ints
+                             .Where(new FilterEvenInts()) // ug, sugar please
+                             .Select(new HalveAnInt(), default(int)); // ug, sugar please + better type inference...
+            var sum = 0;
+            foreach (var i in enumerable)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Benchmark/ValueLambdas/WhereSelect/Benchmark.StructLinq.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using StructLinq;
+
+namespace Cistern.Benchmarks.ValueLambdas.WhereSelect
+{
+    partial class Benchmark
+    {
+        struct WherePredicate : IFunction<int, bool> { public bool Eval(int element) => (element & 1) == 0; }
+        struct SelectFunction : IFunction<int, int> { public int Eval(int element) => element / 2; }
+
+        [Benchmark]
+        public int StructLinq()
+        {
+            return _ints switch
+            {
+                int[] asArray => AsArray(asArray),
+                List<int> asList => AsList(asList),
+                var x => AsEnumerable(x)
+            };
+
+            static int AsList(List<int> list)
+            {
+                var where = new WherePredicate();
+                var select = new SelectFunction();
+
+                var enumerable = list
+                                      .ToStructEnumerable()
+                                      .Where(ref @where, x => x)
+                                      .Select(ref @select, x => x, x => x);
+                var sum = 0;
+                foreach (var i in enumerable)
+                {
+                    sum += i;
+                }
+                return sum;
+            }
+
+            static int AsArray(int[] array)
+            {
+                var where = new WherePredicate();
+                var select = new SelectFunction();
+
+                var enumerable = array
+                                 .ToStructEnumerable()
+                                 .Where(ref @where, x => x)
+                                 .Select(ref @select, x => x, x => x);
+                var sum = 0;
+                foreach (var i in enumerable)
+                {
+                    sum += i;
+                }
+                return sum;
+            }
+
+            static int AsEnumerable(IEnumerable<int> enumerable)
+            {
+                var where = new WherePredicate();
+                var select = new SelectFunction();
+
+                var structEnum = enumerable
+                                 .ToStructEnumerable()
+                                 .Where(ref @where, x => x)
+                                 .Select(ref @select, x => x, x => x);
+                var sum = 0;
+                foreach (var i in structEnum)
+                {
+                    sum += i;
+                }
+                return sum;
+            }
+        }
+    }
+}
