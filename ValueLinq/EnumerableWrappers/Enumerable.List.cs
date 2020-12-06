0bf2849ad4f53401feff855509a2594d9ca478fb
Paul Westcott <p.westcott@gmail.com>
2020-12-06 15:17:32 +1100

    Fixed up the non-shared List functions

--- ValueLinq/EnumerableWrappers/Enumerable.List.cs
+++ ValueLinq/EnumerableWrappers/Enumerable.List.cs
@@ -8,16 +8,12 @@ namespace Cistern.ValueLinq
 {
     public static partial class ValueLinqList
     {
-
         public static bool Any<TSource>(this List<TSource> source) => source.Count() > 0;
 
+        public static TSource Last<TSource>(this List<TSource> source) => source[source.Count - 1];
+        public static TSource LastOrDefault<TSource>(this List<TSource> source) => source.Count == 0 ? default : source[source.Count - 1];
 
-        // --
-
-        public static TSource Last<TSource>(this List<TSource> source) => source.OfList().Last();
-        public static TSource LastOrDefault<TSource>(this List<TSource> source) => source.OfList().LastOrDefault();
-
-        public static List<T> ToList<T>(this List<T> source) => source.OfList().ToList();
+        public static List<T> ToList<T>(this List<T> source) => new (source);
 
         public static int Count<T>(this List<T> inner) => inner.Count;
 
@@ -38,9 +34,9 @@ namespace Cistern.ValueLinq
             return source.OfList().SelectMany(selector);
         }
 
-        public static IEnumerable<T> Concat<T>(this List<T> first, List<T> second) => first.OfList().Concat(second.OfList());
+        public static IEnumerable<T> Concat<T>(this List<T> first, List<T> second) => new ValueEnumerable<T, ConcatNode<T, ListNode<T>, ListNode<T>>>(NodeImpl.Concat<T, ListNode<T>, ListNode<T>>(Enumerable.ToNode(first), Enumerable.ToNode(second)));
 
-        public static TSource ElementAt<TSource>(this List<TSource> source, int index) => source.OfList().ElementAt(index);
-        public static TSource ElementAtOrDefault<TSource>(this List<TSource> source, int index) => source.OfList().ElementAtOrDefault(index);
+        public static TSource ElementAt<TSource>(this List<TSource> source, int index) => source[index];
+        public static TSource ElementAtOrDefault<TSource>(this List<TSource> source, int index) => index >= source.Count ? default : source[index];
     }
 }
