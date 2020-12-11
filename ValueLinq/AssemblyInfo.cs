9a90531692fb9b873c9686813179226bc1f050fd
Paul Westcott <p.westcott@gmail.com>
2020-12-12 06:45:59 +1100

    Not 100% convinced it provides benefit, but trialling SkipLocalsInit

---
+++ ValueLinq/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Runtime.InteropServices;
+
+// In SDK-style projects such as this one, several assembly attributes that were historically
+// defined in this file are now automatically added during build and populated with
+// values defined in project properties. For details of which attributes are included
+// and how to customise this process see: https://aka.ms/assembly-info-properties
+
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM
+// components.  If you need to access a type in this assembly from COM, set the ComVisible
+// attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
+
+[assembly: Guid("15aa0269-d7c7-4e45-a8e3-bbc5f77df6c3")]
+
+[module: System.Runtime.CompilerServices.SkipLocalsInit]
